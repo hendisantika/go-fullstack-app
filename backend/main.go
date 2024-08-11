64ab9bb626185fbbfb30290701a01467b8529125
hendisantika <hendisantika@yahoo.co.id>
2024-08-11 22:09:25 +0700

    Add enableCORS func 🫸🌀✏️📗🐧🐳

--- backend/main.go
+++ backend/main.go
@@ -43,3 +43,22 @@ func main() {
 	// start server
 	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
 }
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Check if the request is for CORS preflight
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next middleware (or final handler)
+		next.ServeHTTP(w, r)
+	})
+
+}
