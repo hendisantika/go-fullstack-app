0943cf743174cc7a5ef56599af78a95b2d85cf92
hendisantika <hendisantika@yahoo.co.id>
2024-08-11 22:09:42 +0700

    Add jsonContentTypeMiddleware func 🫸🌀✏️📗🐧🐳

--- backend/main.go
+++ backend/main.go
@@ -62,3 +62,11 @@ func enableCORS(next http.Handler) http.Handler {
 	})
 
 }
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set JSON Content-Type
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
