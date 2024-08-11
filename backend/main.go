4bd868926fad22411679f778893b100817a7435e
hendisantika <hendisantika@yahoo.co.id>
2024-08-11 22:11:02 +0700

    Add deleteUser func 🫸🌀✏️📗🐧🐳

--- backend/main.go
+++ backend/main.go
@@ -155,3 +155,27 @@ func updateUser(db *sql.DB) http.HandlerFunc {
 		json.NewEncoder(w).Encode(updatedUser)
 	}
 }
+
+// delete user
+func deleteUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var u User
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.Id, &u.Name, &u.Email)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			_, err := db.Exec("DELETE FROM users WHERE id = $1", id)
+			if err != nil {
+				//todo : fix error handling
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			json.NewEncoder(w).Encode("User deleted")
+		}
+	}
+}
